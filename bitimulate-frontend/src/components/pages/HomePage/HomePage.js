552b22aca936509459ce2757c8f8487b69e6e875
jianmyphy123 <jianmyphy123@tutanota.com>
2018-01-26 18:00:39 +0100

    define block atomic component

--- bitimulate-frontend/src/components/pages/HomePage/HomePage.js
+++ bitimulate-frontend/src/components/pages/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, PageTemplate, PolyBackground } from 'components';
+import { Header, PageTemplate, PolyBackground, Block } from 'components';
 
 export class HomePage extends Component {
   render() {
@@ -8,7 +8,14 @@ export class HomePage extends Component {
         header={<Header />}
       >
         <PolyBackground>
-
+          <Block center shadow>
+            <h1>
+              Please Trade Here - Virtual Crypto Currency Trading Center
+            </h1>
+            <h2>
+              You Can Trade Based On POLONIEX Live Data
+            </h2>
+          </Block>
         </PolyBackground>
       </PageTemplate>
     );
