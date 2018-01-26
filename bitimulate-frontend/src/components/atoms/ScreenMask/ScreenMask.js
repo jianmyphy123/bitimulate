4731885a1432f65f4878b4b9a3b157fa421c1ef8
jianmyphy123 <jianmyphy123@tutanota.com>
2018-01-26 03:34:29 +0100

    created z-index helper function

---
+++ bitimulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Responsive.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const ScreenMask = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ScreenMask;
